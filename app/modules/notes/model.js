103df44cd3cb1a7682be9970938b4e7034e91c2d
John Merz <jmerz826@gmail.com>
2025-10-25 23:45:07 -0700

    add NoteModule

---
+++ app/modules/notes/model.js
@@ -0,0 +1,20 @@
+const { Model } = require('app/modules/common')
+
+class NoteModel extends Model {
+  schema() {
+    return {
+      title: {
+        type: String,
+        trim: true,
+        required: true
+      },
+      message: {
+        type: String,
+        trim: true,
+        required: true
+      }
+    }
+  }
+}
+
+module.exports = NoteModel
