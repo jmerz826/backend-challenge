9c2e57d05634b9aca6f339aa1dbb884c4cc54a25
John Merz <jmerz826@gmail.com>
2025-10-25 23:08:03 -0700

    update status route

--- app/api/status/controller.js
+++ app/api/status/controller.js
@@ -1,5 +1,24 @@
+const mongodb = require('app/lib/mongodb')
+
 exports.currentStatus = function (req, res) {
-  res.status(200).send({
-    status: 'OK'
+  const { readyState, ReadyStates } = mongodb
+
+  const isDbConnected = readyState === ReadyStates.connected
+
+  if (isDbConnected) {
+    return res.status(200).send({
+      status: 'OK'
+    })
+  }
+
+  const statusMsg =
+    {
+      [ReadyStates.connecting]: 'db connecting',
+      [ReadyStates.disconnected]: 'db disconnected',
+      [ReadyStates.disconnecting]: 'db disconnecting'
+    }[readyState] || 'Internal Server Error'
+
+  return res.status(500).send({
+    status: statusMsg
   })
 }
