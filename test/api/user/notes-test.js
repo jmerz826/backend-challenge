48053988333915720d252c9cbb301a603ca135a5
John Merz <jmerz826@gmail.com>
2025-10-26 00:18:09 -0700

    read notes test

---
+++ test/api/user/notes-test.js
@@ -0,0 +1,61 @@
+let should
+let agent
+let mockData
+
+before(() => {
+  should = require('should')
+  agent = require('test/lib/agent')
+  mockData = require('test/lib/mock-data')
+})
+
+describe('api', () => {
+  describe('user', () => {
+    describe('notes', () => {
+      let globalAuth
+      let noteService
+
+      before(async () => {
+        globalAuth = await mockData.mockAuthAndUser()
+        noteService = require('app/modules/note')
+
+        await noteService.create({
+          title: 'Test Note 1',
+          message: 'This is first test note',
+          userId: globalAuth.user
+        })
+
+        await noteService.create({
+          title: 'Test Note 2',
+          message: 'This is second test note',
+          userId: globalAuth.user
+        })
+      })
+
+      it('should fail with invalid auth', () => {
+        return agent.client().get(`/user/${globalAuth.user}/notes`).expect(401).promise()
+      })
+
+      it('should list user notes', async () => {
+        const notes = await agent
+          .client()
+          .get(`/user/${globalAuth.user}/notes`)
+          .set('authorization', globalAuth.token)
+          .expect(200)
+          .promise()
+
+        should.exist(notes)
+        notes.should.be.an.Array()
+        notes.length.should.equal(2)
+      })
+
+      it('should throw 403 for valid auth and non-matching userId param', async () => {
+        return agent
+          .client()
+          .get(`/user/${globalAuth.user}123/notes`)
+          .set('authorization', globalAuth.token)
+          .expect(403)
+          .promise()
+      })
+    })
+  })
+})
