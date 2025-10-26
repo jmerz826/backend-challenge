571770c6e4011fefcf1f98239508453baa6161c1
John Merz <jmerz826@gmail.com>
2025-10-26 00:01:02 -0700

    index and findAllByUserId

--- app/modules/note/service.js
+++ app/modules/note/service.js
@@ -1,5 +1,9 @@
 const { Service } = require('app/modules/common')
 
-class NoteService extends Service {}
+class NoteService extends Service {
+  async findAllByUserId(userId) {
+    return this.find({ userId })
+  }
+}
 
 module.exports = NoteService
