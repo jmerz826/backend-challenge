9dfa39bd8715a66bbf0e16c56ff309a959b5e425
John Merz <jmerz826@gmail.com>
2025-10-26 00:12:54 -0700

    move route

--- app/api/note/controller.js
+++
@@ -1,9 +0,0 @@
-const noteService = require('app/modules/note')
-
-/**
- * @method readAll
- */
-exports.readAll = async (req, res) => {
-  const user = await noteService.findAllByUserId(req.params.id)
-  res.status(200).send(user)
-}
--- app/api/user/controller.js
+++ app/api/user/controller.js
@@ -1,4 +1,5 @@
 const userService = require('app/modules/user')
+const noteService = require('app/modules/note')
 
 /**
  * @method read
@@ -14,4 +15,12 @@ exports.read = async (req, res) => {
 exports.update = async (req, res) => {
   const user = await userService.readAndUpdate(req.params.id, req.body)
   res.status(200).send(user)
+}
+
+/**
+ * @method listNotes
+ */
+exports.listNotes = async (req, res) => {
+  const notes = await noteService.findAllByUserId(req.params.id)
+  res.status(200).send(notes)
 }
