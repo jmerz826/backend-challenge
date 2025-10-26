27a162d04fc565c772001c5b625fee1cd02362a7
John Merz <jmerz826@gmail.com>
2025-10-25 23:58:06 -0700

    add userId to note model

--- app/modules/note/model.js
+++ app/modules/note/model.js
@@ -3,6 +3,11 @@ const { Model } = require('app/modules/common')
 class NoteModel extends Model {
   schema() {
     return {
+      userId: {
+        type: String,
+        trim: true,
+        required: true
+      },
       title: {
         type: String,
         trim: true,
