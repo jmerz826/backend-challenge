ca8b1bc293a65771ddbf5f515d4be5b51315a974
John Merz <jmerz826@gmail.com>
2025-10-26 22:09:43 -0700

    add comment

--- app/api/note/controller.js
+++ app/api/note/controller.js
@@ -6,6 +6,7 @@ const noteService = require('app/modules/note')
 exports.create = async (req, res) => {
   const note = await noteService.create({
     ...req.body,
+    // notes are tied to authentiacated user
     userId: req.userId
   })
   res.status(201).send(note)
